6507f8d163f913ea6960389c09514dabe4545c34
uni51 <uni51@users.noreply.github.com>
2020-02-02 12:15:25 +0900

    (4) ToDoアプリのタスク一覧表示機能を作る

--- app/Folder.php
+++ app/Folder.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    //
+    /**
+     * リレーションを定義
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tasks()
+    {
+        return $this->hasMany('App\Task');
+    }
 }
